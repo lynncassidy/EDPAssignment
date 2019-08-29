9c56184674f9b351ab3ea1beba05d55f6268803a
lynnC <lynncassidy91@gmail.com>
2019-08-29 12:12:18 +0100

    Final
    Final Changes

--- EDPAssignmentLynn/AccountScreen.Designer.cs
+++ EDPAssignmentLynn/AccountScreen.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.lblBalance = new System.Windows.Forms.Label();
             this.txtBoxBalance = new System.Windows.Forms.TextBox();
             this.btnDeposit = new System.Windows.Forms.Button();
@@ -35,14 +36,21 @@
             this.txtBoxDeposit = new System.Windows.Forms.TextBox();
             this.txtBoxWithdraw = new System.Windows.Forms.TextBox();
             this.btnLogout = new System.Windows.Forms.Button();
+            this.toolTipBalance = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTipDeposit = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTipDepositBox = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTipWithdraw = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTipWithdrawBox = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTipLogout = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
             // lblBalance
             // 
             this.lblBalance.AutoSize = true;
+            this.lblBalance.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblBalance.Location = new System.Drawing.Point(241, 94);
             this.lblBalance.Name = "lblBalance";
-            this.lblBalance.Size = new System.Drawing.Size(46, 13);
+            this.lblBalance.Size = new System.Drawing.Size(70, 21);
             this.lblBalance.TabIndex = 0;
             this.lblBalance.Text = "Balance";
             // 
@@ -52,24 +60,29 @@
             this.txtBoxBalance.Name = "txtBoxBalance";
             this.txtBoxBalance.Size = new System.Drawing.Size(182, 20);
             this.txtBoxBalance.TabIndex = 1;
+            this.toolTipBalance.SetToolTip(this.txtBoxBalance, "Displays your current balance. ");
             // 
             // btnDeposit
             // 
-            this.btnDeposit.Location = new System.Drawing.Point(235, 180);
+            this.btnDeposit.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeposit.Location = new System.Drawing.Point(225, 180);
             this.btnDeposit.Name = "btnDeposit";
-            this.btnDeposit.Size = new System.Drawing.Size(75, 23);
+            this.btnDeposit.Size = new System.Drawing.Size(99, 34);
             this.btnDeposit.TabIndex = 2;
             this.btnDeposit.Text = "Deposit";
+            this.toolTipDeposit.SetToolTip(this.btnDeposit, "Click here to deposit money into your account.\r\n");
             this.btnDeposit.UseVisualStyleBackColor = true;
             this.btnDeposit.Click += new System.EventHandler(this.OnDepositClicked);
             // 
             // btnWithdraw
             // 
-            this.btnWithdraw.Location = new System.Drawing.Point(235, 242);
+            this.btnWithdraw.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWithdraw.Location = new System.Drawing.Point(222, 233);
             this.btnWithdraw.Name = "btnWithdraw";
-            this.btnWithdraw.Size = new System.Drawing.Size(75, 23);
+            this.btnWithdraw.Size = new System.Drawing.Size(102, 32);
             this.btnWithdraw.TabIndex = 3;
             this.btnWithdraw.Text = "Withdraw";
+            this.toolTipWithdraw.SetToolTip(this.btnWithdraw, "Click here to withdraw an amount");
             this.btnWithdraw.UseVisualStyleBackColor = true;
             this.btnWithdraw.Click += new System.EventHandler(this.OnWithdrawClicked);
             // 
@@ -79,6 +92,7 @@
             this.txtBoxDeposit.Name = "txtBoxDeposit";
             this.txtBoxDeposit.Size = new System.Drawing.Size(182, 20);
             this.txtBoxDeposit.TabIndex = 4;
+            this.toolTipDepositBox.SetToolTip(this.txtBoxDeposit, "Enter the amount you\'d like to deposit here.\r\n");
             // 
             // txtBoxWithdraw
             // 
@@ -86,14 +100,17 @@
             this.txtBoxWithdraw.Name = "txtBoxWithdraw";
             this.txtBoxWithdraw.Size = new System.Drawing.Size(182, 20);
             this.txtBoxWithdraw.TabIndex = 5;
+            this.toolTipWithdrawBox.SetToolTip(this.txtBoxWithdraw, "Enter amount you\'d like to withdraw here.");
             // 
             // btnLogout
             // 
+            this.btnLogout.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLogout.Location = new System.Drawing.Point(57, 387);
             this.btnLogout.Name = "btnLogout";
             this.btnLogout.Size = new System.Drawing.Size(103, 34);
             this.btnLogout.TabIndex = 6;
             this.btnLogout.Text = "Logout";
+            this.toolTipLogout.SetToolTip(this.btnLogout, "Click here to logout user account.");
             this.btnLogout.UseVisualStyleBackColor = true;
             this.btnLogout.Click += new System.EventHandler(this.OnLogoutClicked);
             // 
@@ -101,6 +118,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Honeydew;
             this.Controls.Add(this.btnLogout);
             this.Controls.Add(this.txtBoxWithdraw);
             this.Controls.Add(this.txtBoxDeposit);
@@ -124,5 +142,11 @@
         private System.Windows.Forms.TextBox txtBoxDeposit;
         private System.Windows.Forms.TextBox txtBoxWithdraw;
         private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.ToolTip toolTipBalance;
+        private System.Windows.Forms.ToolTip toolTipDeposit;
+        private System.Windows.Forms.ToolTip toolTipWithdraw;
+        private System.Windows.Forms.ToolTip toolTipDepositBox;
+        private System.Windows.Forms.ToolTip toolTipWithdrawBox;
+        private System.Windows.Forms.ToolTip toolTipLogout;
     }
 }
--- EDPAssignmentLynn/LoginScreen.Designer.cs
+++ EDPAssignmentLynn/LoginScreen.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginScreen));
             this.btnLogin = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
             this.lblEmail = new System.Windows.Forms.Label();
@@ -36,42 +37,52 @@
             this.txtBoxEmail = new System.Windows.Forms.TextBox();
             this.txtBoxPassword = new System.Windows.Forms.TextBox();
             this.toolTipEmail = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTipPassword = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTipLogin = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTipExit = new System.Windows.Forms.ToolTip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(275, 353);
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(302, 353);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.Size = new System.Drawing.Size(75, 36);
             this.btnLogin.TabIndex = 0;
             this.btnLogin.Text = "Login";
+            this.toolTipLogin.SetToolTip(this.btnLogin, "Click here to login to your account.");
             this.btnLogin.UseVisualStyleBackColor = true;
             this.btnLogin.Click += new System.EventHandler(this.OnLoginClicked);
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(555, 353);
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(581, 353);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.Size = new System.Drawing.Size(75, 36);
             this.btnExit.TabIndex = 1;
             this.btnExit.Text = "Exit";
+            this.toolTipExit.SetToolTip(this.btnExit, "Click here to exit.");
             this.btnExit.UseVisualStyleBackColor = true;
             // 
             // lblEmail
             // 
             this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblEmail.Location = new System.Drawing.Point(296, 118);
             this.lblEmail.Name = "lblEmail";
-            this.lblEmail.Size = new System.Drawing.Size(60, 13);
+            this.lblEmail.Size = new System.Drawing.Size(97, 21);
             this.lblEmail.TabIndex = 2;
             this.lblEmail.Text = "Enter Email";
             // 
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblPassword.Location = new System.Drawing.Point(296, 198);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(81, 13);
+            this.lblPassword.Size = new System.Drawing.Size(126, 21);
             this.lblPassword.TabIndex = 3;
             this.lblPassword.Text = "Enter Password";
             // 
@@ -89,11 +100,25 @@
             this.txtBoxPassword.Name = "txtBoxPassword";
             this.txtBoxPassword.Size = new System.Drawing.Size(201, 20);
             this.txtBoxPassword.TabIndex = 5;
+            this.toolTipPassword.SetToolTip(this.txtBoxPassword, "Enter your login password here.\r\n");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(353, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(303, 50);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "The Bank of You";
             // 
             // LoginScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.txtBoxPassword);
             this.Controls.Add(this.txtBoxEmail);
             this.Controls.Add(this.lblPassword);
@@ -116,5 +141,9 @@
         private System.Windows.Forms.TextBox txtBoxEmail;
         private System.Windows.Forms.TextBox txtBoxPassword;
         private System.Windows.Forms.ToolTip toolTipEmail;
+        private System.Windows.Forms.ToolTip toolTipPassword;
+        private System.Windows.Forms.ToolTip toolTipLogin;
+        private System.Windows.Forms.ToolTip toolTipExit;
+        private System.Windows.Forms.Label label1;
     }
 }
